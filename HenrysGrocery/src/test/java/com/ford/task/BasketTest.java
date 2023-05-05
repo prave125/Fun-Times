ac08001256b8250bb399f39593c54679412804ea
PraveenK <kprave333@gmail.com>
2023-05-05 09:33:04 +0530

    Created a TestCase for Product, BasketItem, Here i am checking the condition (Apples have a 10% discount)

--- HenrysGrocery/src/test/java/com/ford/task/BasketTest.java
+++ HenrysGrocery/src/test/java/com/ford/task/BasketTest.java
@@ -6,7 +6,7 @@ import java.time.LocalDate;
 
 import org.junit.jupiter.api.Test;
 
-public class BasketTest{
+public class BasketTest {
 	@Test
 	public void testBuySoupGetBreadDiscount() {
 		Basket basket = new Basket();
@@ -24,5 +24,20 @@ public class BasketTest{
 		assertEquals(0.40, discountAmount, 0.001);
 	}
 
+	@Test
+	public void testApplesGetDiscount() {
+		Basket basket = new Basket();
+		Product apples = new Product("apples", "single", 0.10);
+		Product bread = new Product("bread", "loaf", 0.80);
+		basket.addItem(apples, 3);
+		basket.addItem(bread, 2);
 
+		LocalDate validFrom = LocalDate.now().plusDays(3);
+		LocalDate validTo = LocalDate.now().plusMonths(1).withDayOfMonth(1).minusDays(1);
+
+		Discount discount = new ApplesDiscount(validFrom, validTo);
+		double discountAmount = discount.calculateDiscount(basket);
+
+		assertEquals(0.16, discountAmount, 0.001);
+	}
 }
